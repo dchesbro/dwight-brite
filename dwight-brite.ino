6cc4e2aa1d08411dcd81f2c47d25297d59d74023
Dwight Chesbro <dwight.chesbro@simplymusic.com>
2021-01-11 22:16:01 -0800

    Probably will pull settings out into a separate file.

--- dwight-brite.ino
+++ dwight-brite.ino
@@ -5,26 +5,41 @@
 // ...
 #include <EEPROM.h>
 #include <FastLED.h>
+#include <FastLED_NeoMatrix.h>
 #include <FS.h>
 #include <SPIFFS.h>
 #include <WebServer.h>
 #include <WiFi.h>
 
-// Unknown variables.
-const int output_led = 5;
+// ...
+#include "settings.h"
 
-// Define network variables.
-const char* ssid     = "Dwight Brite";
-const char* password = "password";
+	// Network settings.
+	const char* ssid     = "Dwight Brite";  // WiFi network SSID.
+	const char* password = "password";      // WiFi network password
 
-// Define web server variables.
-String header;
+	// FastLED settings.
+	const uint8_t  led_pin = 14;            // LED ouput pin.
+	const uint8_t  led_brt = 52;            // Default LED brightness (0-255).
+	const uint8_t  led_w   = 40;            // LED matrix width.
+	const uint8_t  led_h   = 30;            // LED matrix height.
+	const uint16_t led_num = led_w * led_h; // Total number of LEDs.
+	const uint16_t led_amp = 4000;          // Max milli-amps of power supply.
+
+	// Unknown settings.
+	const uint8_t output_led = 5;
 
 // ...
-WebServer webServer( 80 );
+WebServer server( 80 );
+
+// ...
+CRGB leds[led_num];
+FastLED_NeoMatrix* matrix = new FastLED_NeoMatrix( leds, led_w, led_h, 1, 1, 
+	NEO_MATRIX_TOP     + NEO_MATRIX_LEFT +
+	NEO_MATRIX_COLUMNS + NEO_MATRIX_ZIGZAG );
 
 /**
- * SPIFFS setup
+ * SPIFFS
  * 
  * Setup ESP32 SPIF filesystem.
  */
@@ -58,9 +73,7 @@ void setup_network() {
 		Serial.print( "OK " );
 
 		// Print local network IP address.
-		IPAddress soft_ap_ip = WiFi.softAPIP();
-		Serial.print( "IP Address: " );
-		Serial.print( soft_ap_ip );
+		Serial.print( "IP Address: " + WiFi.softAPIP().toString() );
 
 	// ...else, print failed and exit.
 	} else {
@@ -78,19 +91,34 @@ void setup_network() {
 void setup_webserv() {
 
 	// Define static resources.
-	webServer.serveStatic( "/css/bootstrap.min.css", SPIFFS, "/css/bootstrap.min.css" );
-	webServer.serveStatic( "/js/jquery-3.5.1.slim.min.js", SPIFFS, "/js/jquery-3.5.1.slim.min.js" );
-	webServer.serveStatic( "/js/bootstrap.bundle.min.js", SPIFFS, "/js/bootstrap.bundle.min.js" );
+	server.serveStatic( "/css/bootstrap.min.css", SPIFFS, "/css/bootstrap.min.css" );
+	server.serveStatic( "/js/jquery-3.5.1.slim.min.js", SPIFFS, "/js/jquery-3.5.1.slim.min.js" );
+	server.serveStatic( "/js/bootstrap.bundle.min.js", SPIFFS, "/js/bootstrap.bundle.min.js" );
 
 	// Define endpoints.
-	webServer.serveStatic( "/", SPIFFS, "/index.html" );
-	webServer.serveStatic( "/index.html", SPIFFS, "/index.html" );
+	server.serveStatic( "/", SPIFFS, "/index.html" );
+	server.serveStatic( "/index.html", SPIFFS, "/index.html" );
 
-	webServer.begin();
+	server.begin();
+}
+
+/**
+ * FastLED
+ */
+void setup_fastled() {
+	FastLED.addLeds<WS2812B, led_pin, GRB>( leds, led_num ).setCorrection( TypicalLEDStrip );
+	
+	/**
+	 * TODO: calulate maximum milliamps.
+	 * 
+	 * FastLED.setMaxPowerInVoltsAndMilliamps( 5, led_amp );
+	 */
+
+	FastLED.setBrightness( led_brt );
 }
 
 void handleWeb() {
-	webServer.handleClient();
+	server.handleClient();
 }
 
 void setup() {
@@ -102,12 +130,22 @@ void setup() {
 	// Start serial output.
 	Serial.begin( 115200 );
 
-	// Setup filesystem, network, and web server.
+	// Setup filesystem, network, web server, and LED matrix.
 	setup_filesys();
 	setup_network();
 	setup_webserv();
+	setup_fastled();
 }
 
 void loop() {
-	webServer.handleClient();
+
+	// ...
+	server.handleClient();
+
+		fill_solid( leds, led_num, CRGB::Red );
+		
+		matrix->setCursor( 0, 0 );
+		matrix->print( ";)" );
+
+	FastLED.show();
 }
